ed34d784b4da6dce6b427b58a16f392f895ab061
Veronicatomilola <54279425+Veronicatomilola@users.noreply.github.com>
2019-11-14 20:56:48 -0500

    add index.php

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+		
+		<link rel="stylesheet" href="Veronica.css" />
+		
+        <title>Veronica EMIOLA</title>
+	</head>
+	<body>
+	
+		<div id="head">
+			<div id="name">
+			 	<h1> EMIOLA VERONICA TOMILOLA </h1>
+				<a id="image" href=" https://res.cloudinary.com/vtomilola/image/upload/v1566942482/samples/IMG_20190814_112259_ubl3ra.jpg"> 
+					<img src="avar.jpg" style="width:50px"/> </a>
+			</div>
+		
+			<div id="contact">
+				<div id="address">
+			  		<p>	Address: House 19, Road 23, Obafemi Awolowo University Senior Staff Quarters, Ile-Ife, Osun State </p>
+				</div>
+				<div id="email">
+			  		<p>	Email: vtomilola@gmail.com Telephone: +2348053726116 </p>
+				</div>
+			</div>
+			<span class="error"> <?php  echo $positive_Message ; echo $negative_Message ?></span> 
+		
+		</div>
+
+		<div id="body">
+			<div id="prof">
+				<p> <h2 class="section_name">PROFFESSIONAL SUMMARY </h2> </p>
+				<p class="pro"> An Estate Surveyor and Valuer, and an Economist, with the ability to work in a diverse multicultural environment,
+					a zeal to learn more, and a drive to make an impact and offer expert knowledge and skills to the best of my ability,
+					for the benefit of my team and my organization. 
+				</p>
+			</div>
+		
+			<div id="comp">
+				<h2 class="section_name">COMPETENCE AND SKILLS</h2> 
+				<ul>
+					<li> HTML (Proficient)
+					<li> CSS (Proficient)
+					<li> Software Usage: Slack, GitHub, Pivotal Tracker, Cloudinary, Microsoft Word,
+				 	Power Point, Microsoft Excel, Online research skills. </li>
+					<li> Experience in Real Estate, Property Management, and Property Valuation. </li>
+					<li> Knowledge of land policies, principles of property valuation, asset valuation,
+				 	advanced valuation, plant and machinery valuation </li>
+					<li> Knowledge of relevant laws (including law of torts, law of contract, arbitration and awards, land law). </li>
+					<li> Knowledge of building construction and materials, building maintenance,management of building projects, 
+				 	building services and equipment, project planning and control. </li>
+					<li> Knowledge of principles of Economics, microeconomic and macroeconomic theories.</li>
+					<li> Knowledge of Environmental Impact Assessment </li>
+					<li> Research </li>
+				</ul>
+			</div>
+
+	
+		
+			<div id="edu">		
+				<h2 class="section_name" > EDUCATION </h2>
+				<h3 class="section_name_2"> Post Graduate Diploma in Economics - Obafemi Awolowo University, Ile-Ife, Nigeria (April 2018 - March 2019) </h3>
+				<p> Background, foundation and understanding of courses such as Microeconomic Theory, Macroeconomic Theory, Basic Econometrics, Mathematical Economics,
+				Banking and Financial Institutions, History of Economic Thought, Statistical Economics, Structure of An Economy, Managerial Economics, Research in Economics, Research and Long Essay on Fiscal Regimes in the Extractive Industry </p>
+
+				<h3 class="section_name_2"> Bachelor of Science, Estate Management - Obafemi Awolowo University, Ile-Ife, Nigeria (Oct 2006 – March 2014) </h3>
+				<p>	Background, foundation and understanding of courses such as land policies, principles of property valuation, asset valuation, advanced valuation, plant and machinery
+				valuation, Rating and Taxation, principles of town and country planning, Environmental Impact Assessment, building construction and materials, building maintenance,
+				management of building projects, building services and equipment, project planning and control, introduction to programming, principles of Economics, microeconomic and
+				macroeconomic theories, Full participation in Students Industrial Work Experience Scheme, Long dissertation writing on Staff Housing Maintenance </p>
+			</div>
+		
+			div id="hobbies">
+				<h2 class="section_name"> Hobbies </h2>
+				<p> Music, Reading, Travelling, and acquiring new knowledge. </p>
+			</div><br>
+		
+			<button id="btn_contact" type="button" onclick="openContactForm()"> Contact Me </button>
+		
+		</div>
+	
+	</body>
+</html>
