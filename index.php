59067c396a25c2fad261979cef2bc5f9950df767
Veronicatomilola <54279425+Veronicatomilola@users.noreply.github.com>
2019-11-14 21:00:42 -0500

    effect changes

--- index.php
+++ index.php
@@ -3,37 +3,37 @@
     <head>
         <meta charset="utf-8" />
 		
-		<link rel="stylesheet" href="Veronica.css" />
+		<link rel="stylesheet" href="https://veronicatomilola.github.io/veronica/Veronica.css" />
 		
         <title>Veronica EMIOLA</title>
-	</head>
+    </head>
+
 	<body>
-	
 		<div id="head">
 			<div id="name">
 			 	<h1> EMIOLA VERONICA TOMILOLA </h1>
 				<a id="image" href=" https://res.cloudinary.com/vtomilola/image/upload/v1566942482/samples/IMG_20190814_112259_ubl3ra.jpg"> 
-					<img src="avar.jpg" style="width:50px"/> </a>
+					<img src="avar.jpg" style="width:50px"/>
+				</a>
 			</div>
 		
 			<div id="contact">
 				<div id="address">
-			  		<p>	Address: House 19, Road 23, Obafemi Awolowo University Senior Staff Quarters, Ile-Ife, Osun State </p>
+			  		<p>	Address: No 7, Sharafadeen Soyege Close, Off Oluwole Akinoso Street, Magodo Phase 2, Lagos State, Nigeria. </p>
 				</div>
 				<div id="email">
 			  		<p>	Email: vtomilola@gmail.com Telephone: +2348053726116 </p>
 				</div>
 			</div>
-			<span class="error"> <?php  echo $positive_Message ; echo $negative_Message ?></span> 
 		
 		</div>
 
 		<div id="body">
 			<div id="prof">
 				<p> <h2 class="section_name">PROFFESSIONAL SUMMARY </h2> </p>
-				<p class="pro"> An Estate Surveyor and Valuer, and an Economist, with the ability to work in a diverse multicultural environment,
+				<p class="pro"> A self-driven individual with knowledge and experience in Estate Surveying and Valuation, Economics, and Web Development; with the ability to work in a diverse multicultural environment,
 					a zeal to learn more, and a drive to make an impact and offer expert knowledge and skills to the best of my ability,
-					for the benefit of my team and my organization. 
+					for the benefit of my team and my organization.
 				</p>
 			</div>
 		
@@ -42,14 +42,18 @@
 				<ul>
 					<li> HTML (Proficient)
 					<li> CSS (Proficient)
+					<li> Javascript (Beginner)
+					<li> PHP/Laravel (Beginner)
 					<li> Software Usage: Slack, GitHub, Pivotal Tracker, Cloudinary, Microsoft Word,
-				 	Power Point, Microsoft Excel, Online research skills. </li>
+					 Power Point, Microsoft Excel, Online research skills.
+					</li>
+					<li> HNG Internship 6 (Finalist)</li>
 					<li> Experience in Real Estate, Property Management, and Property Valuation. </li>
 					<li> Knowledge of land policies, principles of property valuation, asset valuation,
-				 	advanced valuation, plant and machinery valuation </li>
+					 advanced valuation, plant and machinery valuation </li>
 					<li> Knowledge of relevant laws (including law of torts, law of contract, arbitration and awards, land law). </li>
 					<li> Knowledge of building construction and materials, building maintenance,management of building projects, 
-				 	building services and equipment, project planning and control. </li>
+					 building services and equipment, project planning and control. </li>
 					<li> Knowledge of principles of Economics, microeconomic and macroeconomic theories.</li>
 					<li> Knowledge of Environmental Impact Assessment </li>
 					<li> Research </li>
@@ -71,14 +75,15 @@
 				macroeconomic theories, Full participation in Students Industrial Work Experience Scheme, Long dissertation writing on Staff Housing Maintenance </p>
 			</div>
 		
-			div id="hobbies">
+			<div id="hobbies">
 				<h2 class="section_name"> Hobbies </h2>
 				<p> Music, Reading, Travelling, and acquiring new knowledge. </p>
 			</div><br>
 		
-			<button id="btn_contact" type="button" onclick="openContactForm()"> Contact Me </button>
-		
+			<button id="btn_contact" type="button"><a href="contact-form.php"></button>> Contact Me </button>
+			
 		</div>
+		
 	
 	</body>
 </html>
