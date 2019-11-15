38bd29fce73150566a27cc88bc8310c4970f0a4a
Veronicatomilola <54279425+Veronicatomilola@users.noreply.github.com>
2019-11-14 21:55:12 -0500

    additional changes

--- contact.php
+++ contact.php
@@ -107,65 +107,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             .success{ color: green; }
         </style>
     </head>
-    <script>
-		function openContactForm() {
-			document.getElementById("contact_me").style.display = "block";
-		}
-		function verification(){
-			var name = document.forms["formName"]["YourName"];                 
-    		var phone = document.forms["formName"]["YourPhone"];  
-    		var message =  document.forms["formName"]["YourMessage"];
-			var error = document.getElementById("msgText");
-			var nameV= name.value;
-			var phoneV= phone.value;	
-	
-			if (nameV.length <4)                                 
-    		{ 
-        		//window.alert("Please enter your name at least 4 characters"); 
-				error.innerHTML = " Please enter your name at least 4 characters ";
-				error.style.color="red";
-				//error.style.font-size="25";
-        		name.focus(); 
-        		return false;
-    		} 
-			if (isNaN(phoneV) )                                 
-    		{ 
-        		//window.alert("Please enter good phone number ");
-				error.innerHTML = " Please enter good phone number  ";
-				error.style.color="red";
-				//error.style.font-size="25";
-        		phone.focus(); 
-        		return false;
-			}
-			if(phoneV.length <11){
-        		//window.alert("Please enter good phone number ");
-				error.innerHTML = " Please enter good phone number  ";
-				error.style.color="red";
-				//error.style.font-size="25";
-        		phone.focus(); 
-        		return false;
-    		} 
-    		if (message.value.length <20)                                 
-    		{ 
-        		//window.alert("Please your message is too short");
-				error.innerHTML = " Please your message is too short  ";
-				error.style.color="red";
-				//error.style.font-size="25";
-        		message.focus(); 
-        		return false;
-    		}
-			return true; 
-		}
-	</script>
+    <script type=text/javascript>
+    <script src="contact.js"></script>
     <body>
         
         <div id="contact_me" >
             <h2>Contact Me</h2>
             <p>Please fill in this form and your message will be sent to me.</p>
 			<form id="contact_form " name="formName" class="contact animate" action="contact.php" onsubmit="return verification()" method="post" >
-				<div class="imgcontainer">
-					<span onclick="document.getElementById('contact_me').style.display='none'" class="close" title="Close form">&times;</span>
-				</div>
 				<br>
 				<div class="container">
 					<div class="form_container">				
